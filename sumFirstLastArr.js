60dfce41378a3be36c0ef256f5f9c5e4812b3fdd
AlekseyGeorgiev <145491263+AlekseyGeorgiev@users.noreply.github.com>
2023-09-28 11:05:27 +0300

    Removed commetry

--- sumFirstLastArr.js
+++ sumFirstLastArr.js
@@ -1,9 +1,8 @@
 function sumFirstLastArrElement(arr) {
-        // let first = arr[0];
-        // let last = arr[arr.length - 1];
+        
 
        console.log(arr[0] + arr[arr.length - 1]);
-        // console.log(first + last);
+        
 }
 // sumFirstLastArrElement([20, 30, 40]);
 sumFirstLastArrElement([10, 17, 22, 33]);
