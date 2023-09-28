24a6c670f04fb58810ad6a4f940007f993206a85
AlekseyGeorgiev <alekseygeorgiev4@gmail.com>
2023-09-28 10:57:37 +0300

    Added Problem 01 from Arrays

---
+++ sumFirstLastArr.js
@@ -0,0 +1,11 @@
+function sumFirstLastArrElement(arr) {
+        // let first = arr[0];
+        // let last = arr[arr.length - 1];
+
+       console.log(arr[0] + arr[arr.length - 1]);
+        // console.log(first + last);
+}
+// sumFirstLastArrElement([20, 30, 40]);
+sumFirstLastArrElement([10, 17, 22, 33]);
+// sumFirstLastArrElement([11, 58, 69]);
+// sumFirstLastArrElement([5]);
